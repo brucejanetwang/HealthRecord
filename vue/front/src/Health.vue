66156aee9b37734759f0ef173fc3a5682874c583
brucejanetwang <94129057@qq.com>
2019-10-20 23:09:24 +0800

    add vue-router

---
+++ vue/front/src/Health.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div id="div_head">
+      <el-avatar
+        style="vertical-align:middle"
+        src="http://pic1.win4000.com/pic/6/56/85ea1629712.jpg"
+      ></el-avatar>
+      <el-button type="primary" @click="dialogFormVisible = true" round>上报</el-button>
+
+      <el-dialog title="测量上报" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+          <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="Report">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: ""
+      },
+      formLabelWidth: "120px"
+    };
+  },
+  methods: {
+    Report() {
+      this.dialogFormVisible = false;
+      console.log(this.form);
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
--- vue/front/src/main.js
+++ vue/front/src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+
+/*route*/
+import VueRoute from 'vue-router'
+import healthPage from './Health.vue'
+import historyPage from './History.vue'
+import homePage from './Home.vue'
+
+const route = new VueRoute({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { name: 'Home', path: '/', component: homePage },
+    { name: 'Health', path: '/health', component: healthPage },
+    { name: 'History', path: '/history', component: historyPage }
+  ]
+})
+
+
+Vue.use(VueRoute)
+Vue.use(ElementUI)
 new Vue({
   el: '#app',
+  route,
   render: h => h(App)
 })
